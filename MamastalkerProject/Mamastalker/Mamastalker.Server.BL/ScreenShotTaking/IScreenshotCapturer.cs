1e4416a82a8ce256eb36ecb49d81644add509ac4
ita5478 <ittai.paz@gmail.com>
2022-01-22 19:14:31 +0200

    Changed the name of the project's root directory.

---
+++ MamastalkerProject/Mamastalker/Mamastalker.Server.BL/ScreenShotTaking/IScreenshotCapturer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Mamastalker.Server.BL.ScreenShotTaking
+{
+    public interface IScreenshotCapturer
+    {
+        byte[] Capture();
+    }
+}
---
+++ MamastalkerProject/Mamastalker/Mamastalker.Server.WinformsUI/ScreenshotCapture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Mamastalker.Server.BL.ScreenShotTaking;
+
+namespace Mamastalker.Server.BL.ScreenShotTaking
+{
+    public class ScreenshotCapture : IScreenshotCapturer
+    {
+        private ImageConverter _imageConverter;
+
+        public ScreenshotCapture()
+        {
+            _imageConverter = new ImageConverter();
+
+        }
+
+        public byte[] Capture()
+        {
+            Rectangle bounds = Screen.GetBounds(Point.Empty);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+                }
+
+                return (byte[]) _imageConverter.ConvertTo(bitmap, typeof(byte[]));
+            }
+        }
+    }
+}
