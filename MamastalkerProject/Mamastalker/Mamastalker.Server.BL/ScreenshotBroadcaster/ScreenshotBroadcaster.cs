ec1e44e62a1a6c5d4adef102ba24693c81c61693
ita5478 <ittai.paz@gmail.com>
2022-01-23 14:20:41 +0200

    Implementation: Add ScreenshotBroadcaster that implements IBroadcaster.

---
+++ MamastalkerProject/Mamastalker/Mamastalker.Server.BL/ScreenshotBroadcaster/ScreenshotBroadcaster.cs
@@ -0,0 +1,45 @@
+﻿using Mamastalker.Server.BL.Broadcasting;
+using Mamastalker.Server.BL.ScreenShotTaking;
+using System.Threading;
+using System;
+using System.Threading.Tasks;
+
+namespace Mamastalker.Server.BL.ScreenshotBroadcaster
+{
+    public class ScreenshotBroadcaster : IBroadcaster
+    {
+        private delegate void NotifySubscribersCallback(ScreenshotCapturedEventArgs eventArgs);
+        private event NotifySubscribersCallback _notifySubscribersEvent;
+
+        private CancellationToken _broadcastCancellationToken;
+        private IScreenshotCapturer _screenshotCapturer;
+        private int _broadcastIntervals;
+
+        public ScreenshotBroadcaster(CancellationToken broadcastCancellationToken, IScreenshotCapturer screenshotCapturer, int broadcastIntervals)
+        {
+            _broadcastCancellationToken = broadcastCancellationToken;
+            _screenshotCapturer = screenshotCapturer;
+            _broadcastIntervals = broadcastIntervals;
+        }
+
+        public async Task Broadcast()
+        {
+            while (!_broadcastCancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(_broadcastIntervals);
+
+                if((_notifySubscribersEvent?.GetInvocationList().Length ?? 0) > 1)
+                {
+                    var imageBytes = _screenshotCapturer.Capture();
+
+                    _notifySubscribersEvent.Invoke(new ScreenshotCapturedEventArgs(DateTime.Now, imageBytes));
+                }
+            }
+        }
+
+        public void Subscribe(IBroadcastSubscriber subscriber)
+        {
+            _notifySubscribersEvent += subscriber.Notify;
+        }
+    }
+}
