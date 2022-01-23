e2488db8b806ce096c4436f4e51f9967ea749f0f
ita5478 <ittai.paz@gmail.com>
2022-01-23 14:03:14 +0200

    Addition: Add IBroadcaster abstraction.

---
+++ MamastalkerProject/Mamastalker/Mamastalker.Server.BL/Broadcasting/IBroadcaster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mamastalker.Server.BL.Broadcasting
+{
+    public interface IBroadcaster
+    {
+        void Subscribe(IBroadcastSubscriber subscriber);
+
+        Task Broadcast();
+    }
+}
