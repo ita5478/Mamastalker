3f74e9f55f7d3eb2a3d58f07f3782dfcc356b2f4
ita5478 <ittai.paz@gmail.com>
2022-01-23 13:55:10 +0200

    Addition: Add ScreenshotCapturedEventArgs object.

---
+++ MamastalkerProject/Mamastalker/Mamastalker.Server.BL/ScreenShotTaking/ScreenshotCapturedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mamastalker.Server.BL.ScreenShotTaking
+{
+    public class ScreenshotCapturedEventArgs : EventArgs
+    {
+        public readonly DateTime CaptureTime;
+
+        public readonly byte[] ImageBytes;
+
+        public ScreenshotCapturedEventArgs(DateTime captureTime, byte[] imageBytes)
+        {
+            ImageBytes = imageBytes;
+            CaptureTime = captureTime;
+        }
+    }
+}
