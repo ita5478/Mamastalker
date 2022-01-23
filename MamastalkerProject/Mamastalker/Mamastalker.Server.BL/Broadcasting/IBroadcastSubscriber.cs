4ffadda9a54cc6587d293580f650ad554326cc68
ita5478 <ittai.paz@gmail.com>
2022-01-23 14:02:41 +0200

    Addition: Add IBroadcastSubscriber Abstraction.

---
+++ MamastalkerProject/Mamastalker/Mamastalker.Server.BL/Broadcasting/IBroadcastSubscriber.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mamastalker.Server.BL.Broadcasting
+{
+    public interface IBroadcastSubscriber
+    {
+        void Notify(EventArgs eventArgs);
+    }
+}
